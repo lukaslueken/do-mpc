274ae63e26bef0535f266e4cc760a8d4907e2639
Lukas Lüken <lukas.lueken@tu-dortmund.de>
2023-01-26 16:00:50 +0100

    finite differences validation of sensitivitiy calculation (dxdp)

--- do_mpc/differentiator/_nlpdifferentiator.py
+++ do_mpc/differentiator/_nlpdifferentiator.py
@@ -324,7 +324,48 @@ class NLPDifferentiator:
 
         return sens_num
 
+def validate_fd(sens_vals, nlp_solver, nlp_bounds, p_num, x0, n_eval = 10, step_size = 1e-3):
+    
+    # define function to solve nlp for various bounds
+    def solve_nlp(S,nlp_bounds, p_num, x0):
+        # solve NLP
+        if "lbx" in nlp_bounds.keys() and "ubx" in nlp_bounds.keys():
+            sol = S(x0=x0, p=p_num, lbx=nlp_bounds["lbx"], ubx=nlp_bounds["ubx"], lbg=nlp_bounds["lbg"], ubg=nlp_bounds["ubg"])
+        else:
+            sol = S(x0=x0, p=p_num, lbg=nlp_bounds["lbg"], ubg=nlp_bounds["ubg"])
+        return sol
+    
+    n_p = p_num.shape[0]
+    n_x = x0.shape[0]
+    
+    param_sens = sens_vals[0:n_x,0:n_p]
+    
+    dp_unscaled = 2*(np.random.rand(n_p,n_eval)-0.5)
+    dp_len = np.linalg.norm(dp_unscaled,axis=0)
+    dp = step_size*dp_unscaled/dp_len
+    
+    p_fd = p_num.reshape((-1,1))+dp    
+    dopt = param_sens@dp
+    
+    r_ca_old = solve_nlp(nlp_solver, nlp_bounds, p_num, x0)
+    r_ca_new = solve_nlp(nlp_solver, nlp_bounds, p_fd, r_ca_old["x"]+dopt)
+    # r_ca_new = solve_nlp(nlp_solver, nlp_bounds, p_fd, x0)
+    
+    x_old = np.array(r_ca_old["x"])
+    x_new = np.array(r_ca_new["x"])
+    
+    diff_x = x_new - x_old
+    abs_dev = np.abs(diff_x - dopt)
 
+    eval_dict = {}
+    eval_dict["max"] = np.max(abs_dev)
+    eval_dict["rel_max"] = np.max(abs_dev)/step_size
+    eval_dict["mean"] = np.mean(abs_dev)
+    eval_dict["rel_mean"] = np.mean(abs_dev)/step_size
+    eval_dict["std"] = np.std(abs_dev)
+    eval_dict["rel_std"] = np.std(abs_dev)/step_size
+    
+    return eval_dict
     
 def setup_NLP_example_1():
     # build NLP
@@ -358,8 +399,8 @@ def setup_NLP_example_1():
     ## setup Bounds
     lbg = -np.inf*np.ones(g_sym.shape)
     ubg = np.zeros(g_sym.shape)
-    ubg[-1] = 1.2
-    lbg[-1] = 1.0
+    # ubg[-1] = 1.2
+    # lbg[-1] = 1.0
 
     # lbx = np.zeros(x_sym.shape)
     lbx = -np.inf*np.ones(x_sym.shape)
@@ -404,8 +445,20 @@ if __name__ == '__main__':
 
     if True:
         # Test 1
-        p_num = np.array((1,2))
-        S0 = nlpsol('S', 'ipopt', nlp)
+        p_num = np.array((1,1))
+        # S0 = nlpsol('S', 'ipopt', nlp)
+
+        nlp_sol_opts = {}
+        ipopt_options = {"fixed_variable_treatment": "make_constraint"} # FIXME: very important to get correct lagrange multipliers # TODO: set this as default in do-mpc
+        nlp_sol_opts["expand"] = False
+
+        ipopt_options["print_level"] = 4
+
+        nlp_sol_opts["ipopt"] = ipopt_options
+        # nlp_sol_opts["expand"] = False
+
+        S0 = nlpsol('S', 'ipopt', nlp,nlp_sol_opts) #,nlpsol_options=ipopt_options
+
         r0 = S0(x0=0, p=p_num, **nlp_bounds)
 
         z_num, where_lam_not_zero = nlp_diff.extract_primal_dual_solution(r0, eps=1e-6)
@@ -413,7 +466,9 @@ if __name__ == '__main__':
         dx_dp = nlp_diff.map_dxdp(param_sens)
         dlam_dp = nlp_diff.map_dlamdp(param_sens, where_lam_not_zero)
 
-        
+    if True:
+        eval_dict = validate_fd(dx_dp, S0, nlp_bounds, p_num, x0=r0["x"], n_eval = 1, step_size = 1e-3)
+        print(eval_dict)
     assert 1==2
         
         
