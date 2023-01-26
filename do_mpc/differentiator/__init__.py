5deed8ab1b4c02d90e7b2d7139e088c57009fcd9
Lukas Lüken <lukas.lueken@tu-dortmund.de>
2023-01-26 14:18:21 +0100

    first draft of fast sensitivity calculation method based on IFT

--- do_mpc/differentiator/__init__.py
+++ do_mpc/differentiator/__init__.py
@@ -5,5 +5,5 @@ Tools for NLP differentiation.
     This module is not fully implemented and experimental.
 """
 
-from ._nlphandler import NLPHandler
+# from ._nlphandler import NLPHandler
 from ._nlpdifferentiator import NLPDifferentiator
