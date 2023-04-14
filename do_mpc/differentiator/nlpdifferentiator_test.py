0cf79227982e198db3c5445b293b25c4a3b97ebf
Lukas Lüken <lukas.lueken@tu-dortmund.de>
2023-04-14 10:21:56 +0200

    REFACTOR: nlp differentiator test

--- do_mpc/differentiator/nlpdifferentiator_test.py
+++ do_mpc/differentiator/nlpdifferentiator_test.py
@@ -1,6 +1,96 @@
-from _nlpdifferentiator import NLPDifferentiator, setup_NLP_example_1, validate_fd
+from _nlpdifferentiator import NLPDifferentiator#, setup_NLP_example_1, validate_fd
 import casadi as ca
 import numpy as np
+
+
+def validate_fd(sens_vals, nlp_solver, nlp_bounds, p_num, x0, n_eval = 10, step_size = 1e-3):
+    
+    # define function to solve nlp for various bounds
+    def solve_nlp(S,nlp_bounds, p_num, x0):
+        # solve NLP
+        if "lbx" in nlp_bounds.keys() and "ubx" in nlp_bounds.keys():
+            sol = S(x0=x0, p=p_num, lbx=nlp_bounds["lbx"], ubx=nlp_bounds["ubx"], lbg=nlp_bounds["lbg"], ubg=nlp_bounds["ubg"])
+        else:
+            sol = S(x0=x0, p=p_num, lbg=nlp_bounds["lbg"], ubg=nlp_bounds["ubg"])
+        return sol
+    
+    n_p = p_num.shape[0]
+    n_x = x0.shape[0]
+    
+    param_sens = sens_vals[0:n_x,0:n_p]
+    
+    dp_unscaled = 2*(np.random.rand(n_p,n_eval)-0.5)
+    dp_len = np.linalg.norm(dp_unscaled,axis=0)
+    dp = step_size*dp_unscaled/dp_len
+    
+    p_fd = p_num.reshape((-1,1))+dp    
+    dopt = param_sens@dp
+    
+    r_ca_old = solve_nlp(nlp_solver, nlp_bounds, p_num, x0)
+    r_ca_new = solve_nlp(nlp_solver, nlp_bounds, p_fd, r_ca_old["x"]+dopt)
+    # r_ca_new = solve_nlp(nlp_solver, nlp_bounds, p_fd, x0)
+    
+    x_old = np.array(r_ca_old["x"])
+    x_new = np.array(r_ca_new["x"])
+    
+    diff_x = x_new - x_old
+    abs_dev = np.abs(diff_x - dopt)
+
+    eval_dict = {}
+    eval_dict["max"] = np.max(abs_dev)
+    eval_dict["rel_max"] = np.max(abs_dev)/step_size
+    eval_dict["mean"] = np.mean(abs_dev)
+    eval_dict["rel_mean"] = np.mean(abs_dev)/step_size
+    eval_dict["std"] = np.std(abs_dev)
+    eval_dict["rel_std"] = np.std(abs_dev)/step_size
+    
+    return eval_dict
+    
+def setup_NLP_example_1():
+    # build NLP
+    # https://web.casadi.org/blog/nlp_sens/
+
+    nlp_id = "casadi_nlp_sens_adapted"
+    
+    ## Decision Variables
+    x_sym = ca.SX.sym('x',2,1)
+
+    ## Parameters
+    p_sym = ca.SX.sym('p',2,1)
+
+    ## Objective Function
+    f_sym = (p_sym[0] - x_sym[0])**2 + 0.2*(x_sym[1] - x_sym[0]**2)**2
+
+    ## Constraint Functions
+    # ubg = 0 (standard form)
+
+    g_0 = (p_sym[1]**2)/4 - (x_sym[0]+0.5)**2 + x_sym[1]**2
+    g_1 = (x_sym[0]+0.5)**2 + x_sym[1]**2- p_sym[1]**2
+    g_2 = (x_sym[0]+0.5)
+
+    # concat constraints
+    g_sym = ca.vertcat(g_0,g_1,g_2)
+
+    ## setup NLP
+    nlp = {'x':x_sym, 'p':p_sym, 'f':f_sym, 'g':g_sym}
+
+
+    ## setup Bounds
+    lbg = -np.inf*np.ones(g_sym.shape)
+    ubg = np.zeros(g_sym.shape)
+    # ubg[-1] = 1.2
+    # lbg[-1] = 1.0
+
+    # lbx = np.zeros(x_sym.shape)
+    lbx = -np.inf*np.ones(x_sym.shape)
+    ubx = np.inf*np.ones(x_sym.shape)
+
+    # lbx[0] = 0.0
+    # ubx[0] = 0.0
+
+    nlp_bounds = {"lbg": lbg,"ubg": ubg,"lbx": lbx,"ubx": ubx}
+    
+    return nlp, nlp_bounds, nlp_id
 
 # setup NLP
 nlp, nlp_bounds, nlp_id = setup_NLP_example_1()
