be332e7b7f4a5db6a0866a3131f483d8cbd102f3
Lukas Lüken <lukas.lueken@tu-dortmund.de>
2023-04-15 12:58:22 +0200

    Testcase for base class NLPDiff adapted, default lin-solver=None

--- do_mpc/differentiator/nlpdifferentiator_test.py
+++ do_mpc/differentiator/nlpdifferentiator_test.py
@@ -100,6 +100,11 @@ nlp_dict = {"nlp": nlp, "nlp_bounds": nlp_bounds}
 nlp_diff = NLPDifferentiator(nlp_dict)
 
 nlp_diff.settings.check_LICQ = True
+nlp_diff.settings.check_rank = True
+nlp_diff.settings.track_residuals = True
+nlp_diff.settings.active_set_tol = 1e-6
+nlp_diff.settings.set_lam_zero = False
+nlp_diff.settings.lstsq_fallback = False
 
 print(nlp_diff.settings)
 
@@ -122,20 +127,13 @@ p_num = np.array((0,1,1))
 nlp_sol = nlp_solver(x0=0, p=p_num, **nlp_bounds)
 nlp_sol["p"] = p_num
 
-# get_do_mpc_nlp_sol
-# if nlp_diff.flags["reduced_nlp"]:
-#     nlp_sol_red = nlp_diff.reduce_nlp_solution_to_determined(nlp_sol)
-# else:
-#     nlp_sol_red = nlp_sol
-
-# z_num, where_cons_active = nlp_diff._extract_active_primal_dual_solution(nlp_sol)
-# z_num, where_cons_active = nlp_diff._extract_active_primal_dual_solution(nlp_sol_red)
-
-# param_sens, residues, LICQ_status = nlp_diff.calculate_sensitivities(z_num, p_num, where_cons_active)
-# dx_dp_num, dlam_dp_num = nlp_diff.map_param_sens(param_sens, where_cons_active)
-
 dx_dp_num, dlam_dp_num, residuals, LICQ_status, where_cons_active = nlp_diff.differentiate(nlp_sol)
 
-if True:
+# print residuals, LICQ_statusm where_cons_active
+print("residuals: ", residuals)
+print("LICQ_status: ", LICQ_status)
+print("where_cons_active: ", where_cons_active)
+
+if False:
     eval_dict = validate_fd(dx_dp_num, nlp_solver, nlp_bounds, p_num, x0=nlp_sol["x"], n_eval = 10, step_size = 1e-3)
     print(eval_dict)
