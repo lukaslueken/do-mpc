d28be551fd22a014044c3a79a47576ef0496070f
Lukas Lüken <lukas.lueken@tu-dortmund.de>
2023-04-16 12:12:04 +0200

    First draft inheritance dompc diff

--- do_mpc/differentiator/__init__.py
+++ do_mpc/differentiator/__init__.py
@@ -3,6 +3,6 @@ Tools for NLP differentiation.
 """
 
 # from ._nlphandler import NLPHandler
-from ._nlpdifferentiator import NLPDifferentiator
+from ._nlpdifferentiator import NLPDifferentiator, DoMPCDifferentiatior
 # from ._nlpdifferentiator import build_sens_sym_struct
 # from ._nlpdifferentiator import assign_num_to_sens_struct
